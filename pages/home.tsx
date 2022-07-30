9a7df588d3494c541fb4fda8f3f1d7088884a21e
Radivoje Katanic <k.rade313@gmail.com>
2022-07-30 18:43:07 +0200

    Adds date pickers for fetching data by date range

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,20 +1,24 @@
-import { useEffect, useState } from "react";
+import { EffectCallback, useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { useAuth } from "../context/AuthUserContext";
 import TransactionTable from "../components/TransactionsTable";
 import { getAllTransactionsInDateRange } from "../api/transactionsApi";
 import Overview from "../components/Overview";
 import Actions from "../components/Actions";
+import Input from "../components/Input";
+import ReloadIcon from "../assets/icons/reload.svg";
+import IconButton from "../components/IconButton";
 
 var date = new Date(),
   year = date.getFullYear(),
   month = date.getMonth();
-var firstDay = new Date(year, month, 1).getTime();
-var lastDay = new Date(year, month + 1, 0).getTime();
+var firstDay = new Date(year, month, 1);
+var lastDay = new Date();
 
-const Home = () => {
+const Home = (): JSX.Element | null => {
   const { authUser } = useAuth();
   const router = useRouter();
+
   const [data, setData] = useState({
     transactions: [],
     income: {
@@ -27,12 +31,13 @@ const Home = () => {
       percentage: 0,
     },
   });
+
   const [{ startDate, endDate }, setDateRange] = useState({
     startDate: firstDay,
     endDate: lastDay,
   });
 
-  useEffect((): void => {
+  useEffect((): ReturnType<EffectCallback> => {
     if (!authUser) router.push("/");
   }, [authUser, router]);
 
@@ -41,16 +46,37 @@ const Home = () => {
   }, []);
 
   const fetchTransactions = async (): Promise<void> => {
-    const res = await getAllTransactionsInDateRange(startDate, endDate);
+    const res = await getAllTransactionsInDateRange(
+      startDate.getTime(),
+      endDate.getTime()
+    );
     setData({ transactions: res.data, ...res });
-    console.log(data);
   };
 
   if (!authUser) return null;
 
   return (
     <div className="home">
-      <Actions />
+      <div className="home-actions">
+        <Actions />
+        <div className="home-date-range">
+          <IconButton icon={<ReloadIcon />} onClick={fetchTransactions} />
+          <Input
+            type="date"
+            onChange={(e) =>
+              setDateRange({ endDate, startDate: new Date(e.target.value) })
+            }
+            value={startDate.toISOString().substring(0, 10)}
+          />
+          <Input
+            type="date"
+            onChange={(e) =>
+              setDateRange({ startDate, endDate: new Date(e.target.value) })
+            }
+            value={endDate.toISOString().substring(0, 10)}
+          />
+        </div>
+      </div>
       <Overview
         income={data.income}
         expense={data.expense}
